9dad248b694578ac68abcb611675ebad2633bcda
Aaron Magil <aaron@societyofgrownups.com>
2015-09-26 11:28:35 -0400

    Phoenix tutorial.
    Initial commit, up through two simple endpoints

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,12 @@
+defmodule HelloPhoenix.HelloController do
+  use HelloPhoenix.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def show(conn, %{"id" => messenger}) do
+    render conn, :show, messenger: messenger
+  end
+
+end
